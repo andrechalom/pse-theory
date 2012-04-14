4dac815e1a4b958cbea6f78abef6e939277fe94e
Andre Chalom <andrechalom@gmail.com>
2012-04-14 03:18:27 +0000

    Criacao de arquivos para paper com a Kika, organizacao da colaboracao com Lucaix, alteracoes menores no pse.R
    git-svn-id: svn://www.lage.ib.usp.br/chalom/pse@98 3f9c3246-d5c1-4856-9bbf-d06180df4c92

--- R/pse.R
+++ R/pse.R
@@ -9,7 +9,7 @@ modelRun <- function (Xo, r, K, Time) {
 print(modelRun(1,2,3,4))
 
 # Descrevendo o espaco de parametros e gerando o hipercubo
-N <- 200
+N <- 100
 # r eh uniformemente distribuido entre 0 e 2
 rlimits <- 0:N / N*(2-0) + 0
 rpos <- array(0)
@@ -29,15 +29,31 @@ print(res)
 X <- data.frame(rpos,kpos, res)
 
 # Plots e analises
+# Plot de sobrevivencia
 plot(0,0, xlim=c(0,2),ylim=c(10,50),xlab="Valores de r",ylab="Valores de K",main="Sobrevivencia")
 points(X$rpos[X$res>0.001],X$kpos[X$res>0.001], pch=0)
 points(X$rpos[X$res<=0.001],X$kpos[X$res<=0.001], pch=1)
 segments(1,10,1,50,lty=2)
 
+# Plot da populacao final
 Max <- max(X$res)
 Min <- min(X$res)
 plot(0,0, xlim=c(0,2),ylim=c(10,50),xlab="Valores de r",ylab="Valores de K",main="Pop Final")
-points(X$rpos,X$kpos,col=rgb(X$res/(Max-Min),0,0),pch=19)
+points(X$rpos,X$kpos,col=rgb((X$res-Min)/(Max-Min),0,0),pch=19)
+
+# Geramos um modelo linear para prever a populacao em qualquer ponto
+# Ele claramente falha ao identificar a superficie de sobrevivencia
+modellm <- lm (X$res ~ X$rpos + X$kpos)
+print (modellm)
+print (summary(modellm))
+rpred <- rep(1:N/N*(2-0) + 0 - (1/N),N)
+kpred <- rep(1:N/N*(50-10) + 10 - 1/N*(50-10)/2, each=N)
+prd <- modellm$coefficients[1] + modellm$coefficients[2]*rpred + modellm$coefficients[3]*kpred
+# Normalizacao, jogando fora < 0 
+prd <- prd / max(prd)
+prd[prd < 0] <- 0
+plot(0,0, xlim=c(0,2),ylim=c(10,50),xlab="Valores de r",ylab="Valores de K",main="Pop Final Predita") 
+points(rpred,kpred,col=rgb(prd,0,0),pch=19)
 
 # Correlacao entre k e r
-cor(kpos,rpos)
+print(cor(kpos,rpos))
