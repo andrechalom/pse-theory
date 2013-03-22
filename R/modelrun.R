e3fa3d34fd40e4ad75a36c23a7d4581632515bf6
Andre Chalom <andrechalom@gmail.com>
2013-03-22 01:46:48 +0000

    X
    git-svn-id: svn://www.lage.ib.usp.br/chalom/pse@901 3f9c3246-d5c1-4856-9bbf-d06180df4c92

--- R/modelrun.R
+++ R/modelrun.R
@@ -1,33 +1,34 @@
 ### Exemplos de utilizacao do pse:
 source("pse.R")
+set.seed(42)
 ### Modelo numerico: crescimento logistico simples
 # O modelo tem 4 parametros: r, K, populacao inicial e tempo final
 # Vamos estudar qual a influencia de cada um deles
 oneRun <- function (r, K, Xo) {
-		Time<-5;
+		Time<-4;
 	X <- array();
 	X[1] <- Xo; #### VAI SER SOBRESCRITO!
 	for (i in 1:Time) {
 		X[i] <- X[length(X)] + r*X[length(X)]*(1-X[length(X)]/K)
 	}
-	return (X[length(X)])
+	return (X)
+	#     return (X[length(X)])
 }
 modelRun <- function (dados) {
 		mapply(oneRun, dados[,1], dados[,2], dados[,3])
 }
 ### Descrevendo o espaco de parametros e gerando o hipercubo
-# Definicao do numero total de amostras:
-N <- 100
 factors <- c("r", "K", "X0")
-res.names <- paste("Time",1:9)
+res.names <- paste("Time",1:4)
 q <- c("qnorm", "qnorm", "qunif")
-q.arg <- list( list(mean=2, sd=0.05), list(mean=40, sd=1),
+r <- c("rnorm", "rnorm", "runif")
+q.arg <- list( list(mean=1.5, sd=0.5), list(mean=80, sd=1),
 				list(min=1, max=50) )
-
 oneRun(1.2, 12, 5)
-meuLHS <- LHS(modelRun, factors, 100, q, q.arg, res.names)
-matplot(t(get.results(meuLHS)), type='l')
-plotecdf(meuLHS)
 
-p <- sensitivity::pcc(as.data.frame(meuLHS@data), as.vector(meuLHS@res), nboot=50)
-plot(p)
+meuLHS <- LHS(modelRun, factors, 100, q, q.arg, res.names)
+
+matplot(t(get.results(meuLHS)), type='l')
+corPlot(meuLHS)
+plotecdf(meuLHS)
+plotprcc(meuLHS)
--- R/pse.R
+++ R/pse.R
@@ -5,115 +5,152 @@
 # Edicao MAR/2013 - funcionalidades para que a funcao "model" possa retornar
 # um array
 library(sensitivity)
-source("corcorr.R")
-source("sobol.R")
-# Dada uma amostragem gerada por sample*, devolve os limites de plotagem para a variavel
-limits <- function (pos) {
-		return (c(min(pos), max(pos)))
-}
+########################CORCORR
 
-### Plots e analises
-sig <- function (form) {
-		l <- lm(form)
-		if (is.na(coefficients(l)[2])) return (" ");
-		f <- anova(lm(form))$"Pr(>F)"[1]
-		if (f < 0.001) return ("***");
-		if (f < 0.01) return ("**");
-		if (f < 0.05) return ("*");
-		if (f < 0.1) return (".");
-		return (" ");
+# Uso: Para forcar correlacao = 0
+# newvars <- LHScorcorr(vars)
+# vars deve ser uma data.frame ou objeto que pode ser coerced para data.frame
+# O valor de retorno eh sempre um data.frame com os mesmos rownames de vars
+# Para aproximar de uma matriz de correlacao dada:
+# newvars <- LHScorcorr(vars,COR)
+
+system("R CMD SHLIB corcorr.c")
+
+# NAO MEXA nos parametros l e it
+LHScorcorr <- function (vars, COR = matrix(0,dim(vars)[2],dim(vars)[2]), l = 2, eps = 0.0005, it = 1, echo=FALSE, maxIt = 2*sqrt(dim(vars)[1])) {
+		N <- dim(vars)[1]; M <- dim(vars)[2]
+		my.names <- colnames(vars)
+		# Condicao de parada: terminamos a correcao
+		if (l == M + 1) {
+				return (vars);
+		}
+		# Condicao de skip: correlacoes pequenas o suficiente:
+		if (max(abs(cor(vars)[l,1:(l-1)] - COR[l,1:(l-1)])) < eps) {
+				return (LHScorcorr (vars, COR, l = l + 1, eps = eps, it = 1, echo=echo));
+		}
+		# Condicao de parada: iteracao maxima atingida para a mesma variavel
+		if (it > maxIt) { 
+				if (echo==T) print("WARNING: correlacao nao converge para o esperado apos numero maximo de iteracoes");
+				return (LHScorcorr (vars, COR, l = l + 1, eps = eps, it = 1, echo=echo));
+		}
+		if (echo==T) print(paste("INFO: Realizando correcao de correlacao para l =",l,"/",M))
+		# Aqui comeca o trabalho para corrigir as cors da var[,l]
+		# Chamada externa para procedure em C, para corrigir correlacoes em var[,l]:
+		dyn.load("corcorr.so")
+		V <- .C("corcorr", vars=as.double(as.matrix(vars)),cor=as.double(COR), N=as.integer(N), M=as.integer(M), l=as.integer(l), FLAGSTOP=as.integer(0))
+		vars <- as.data.frame(matrix(V$vars, nrow=N, ncol=M))
+		names(vars) <- my.names
+		if (V$FLAGSTOP == 1) { # O procedimento convergiu
+				return(LHScorcorr (vars, COR, l = l + 1, eps = eps, it = 1, echo=echo));
+		} else {
+				# Variavel corrigida, repete o procedimento com a mesma variavel
+				# ateh a correlacao estar pequena o suficiente
+				LHScorcorr(vars,COR,l=l,eps=eps,it=it+1, echo=echo)
+		}
 }
-oneCorPlot <- function(res, var, name, log, first, ...) {
-		# TODO: Melhorar essa funcao que deforma o primeiro da linha!!
-		if (first) par(mar=c(5,2,4,0.5), yaxt='s')
-		else par (mar=c(5,0.5,4,0.5), yaxt='n')
-		plot(var,res, xlab=name, log=log, ylab= "", ...)
+###################################
+### Plots e analises
+oneCorPlot <- function(res, var, name, log, ...) {
+		par (mar=c(4,4,2,0.5))
+		plot(var,res, xlab=name[1], log=log, ylab= name[2], ...)
 		l <- lm(res~var)
 		if (!is.na(coefficients(l)[2])) 
 				abline((lm(res~var)))
-		mtext(paste("Cor:",format(cor(var,res), digits=2), sig(res~var)))
 }
 corPlot <- function (vars, res=NULL, log="", ...) {
-
 		if (class(vars)=="LHS") return(corPlot(vars@data, vars@res, log=log, ...))
+		res <- as.data.frame(res)
+		if (is.null(dim(vars))) {nplots <-1} else {nplots <- dim(vars)[2]}
+		if (is.null(dim(res))) {nplots <-nplots*1} else {nplots <- nplots* dim(res)[2]}
 
-		nl <- floor(sqrt(length(vars)))
-		nc <- ceiling(length(vars)/nl)
+		nl <- floor(sqrt(nplots))
+		nc <- ceiling(nplots/nl)
 		opar <- par(mfrow=c(nl,nc), pch='.', ...)
 		on.exit(par(opar))
+
+		index.var <- 1
+		index.res <- 1
 		for (i in 1:nl) for (j in 1:nc) {
-				index <- (i-1)*nc+j;
-				if (index <= length(vars)) oneCorPlot(res,vars[[index]], names(vars)[index], log, first = (j == 1), ...)
+				oneCorPlot(res[,index.res],vars[, index.var], c(names(vars)[index.var],names(res)[index.res]), log, ...)
+				index.res <- index.res +1
+				if (index.res > dim(res)[2]) {index.res <- 1; index.var <- index.var + 1}
+				if (index.var > dim(vars)[2]) return();
 		}
 }
 
-# Funcao acessoria - nao eh para uso externo
-oneTestPlot <- function(p1, p2, test, convex=FALSE, p1name, p2name) {
-		if (convex) {
-				require(spatstat)
-				W <- owin(c(min(p1[test]),max(p1[test])), c(min(p2[test]),max(p2[test])))
-				P <- ppp(p1[test],p2[test],window=W)
-				cTrue <-convexhull(P)
-				W <- owin(c(min(p1[!test]),max(p1[!test])), c(min(p2[!test]),max(p2[!test])))
-				P <- ppp(p1[!test],p2[!test],window=W)
-				cFalse <- convexhull(P)
-		}
-		plot(0,0,
-			 xlim=limits(p1),ylim=limits(p2),
-			 xlab=paste("Valores de ",p1name),ylab=paste("Valores de ",p2name), type='n')
-		if (convex) {
-				plot(cTrue, add=T, col=cm.colors(1), density=10)
-				plot(cFalse, add=T, col=cm.colors(2)[2], density=10)
-		}
-		points(p1[test],p2[test], pch='+')
-		points(p1[!test],p2[!test], pch='-')
-}
-# Plota o resultado de um teste logico pelo hipercubo
-testPlot <- function(vars, test, convex=FALSE, ...) {
-		l <- length(vars)-1
-		opar <- par(mfrow=c(l,l), ...)
-		on.exit(par(opar))
-		for (i in 1:l) {
-				for (j in (l+1):2) {
-						if (i >= j) {
-								plot.new()
-						} else {
-								oneTestPlot(vars[[j]],vars[[i]],test, convex, names(vars)[j], names(vars)[i])
-						}
-				}
-		}
+#                       Nodeplot: anti-boxplot
+#                         Gilles Pujol 2006
+#   Editado Chalom 2013
+
+plot.pcc <- function(x, ylim = c(-1,1), main=(if ("PCC" %in% names(x)) "PCC" else "PRCC"), ...) {  
+		if ("PCC" %in% names(x)) {
+				nodeplot(x$PCC, ylim = ylim, main=main, ...)
+		}else if ("PRCC" %in% names(x)) {
+				nodeplot(x$PRCC, ylim = ylim, main=main, ...)
+		}  
 }
 
-oneGradPlot <- function (p1, p2, res, p1name, p2name) {
-		Max <- max(res)
-		Min <- min(res)
-		plot(0,0,
-			 xlim=limits(p1),ylim=limits(p2),
-			 xlab=paste("Valores de ",p1name),ylab=paste("Valores de ",p2name))
-		points(p1,p2,col=rgb((res-Min)/(Max-Min),0,0),pch=19)
-}
 
-# Plota o resultado da simulacao pelo hipercubo
-gradPlot <- function(vars, res, ...) {
-		l <- length(vars)-1
-		opar <- par(mfrow=c(l,l), ...)
-		on.exit(par(opar))
-		for (i in 1:l) {
-				for (j in (l+1):2) {
-						if (i >= j) {
-								plot.new()
-						} else {
-								oneGradPlot(vars[[j]],vars[[i]],res, names(vars)[j], names(vars)[i])
-						}
-				}
-		}
-}
 
+nodeplot <- function(x, xlim = NULL, ylim = NULL, labels = TRUE,
+                     col = par("col"), pch = 21, bg = "white",
+                     add = FALSE, at = NULL, ...) {
+  n <- nrow(x)
+  if (is.null(xlim)) {
+    xlim <- c(1, n)
+  }
+  if (is.null(ylim)) {
+    ylim <- c(min(x), max(x))
+  }
+  if (is.null(at)) {
+    at <- 1 : n
+  }
+  if (add) {
+    par(new = TRUE)
+  }
+
+  # axes
+  
+  plot(0, xlim = xlim, ylim = ylim, axes = FALSE,
+       xlab = "", type = "n", ...)
+  if (class(labels) == "logical") {
+    if (labels) {
+      axis(side = 1, at = at, labels = rownames(x))
+    } else {
+      axis(side = 1, at = at, labels = FALSE, tick = FALSE)
+    }
+  } else if (class(labels) == "character") {
+    axis(side = 1, at = at, labels = labels)
+  }
+  axis(side = 2)
+  box()
+
+  # bias
+
+  if ("bias" %in% colnames(x)) {
+    xx <- x[["original"]] - x[["bias"]]
+  } else {
+    xx <- x[["original"]]
+  }
+  
+  # confidence intervals
+
+  if (("min. c.i." %in% colnames(x)) & "max. c.i." %in% colnames(x)) {
+    for (i in 1 : n) {
+      lines(c(at[i], at[i]), c(x[["min. c.i."]][i], x[["max. c.i."]][i]),
+            col = col)
+    }
+  }
+
+  # points
+
+  points(at, xx, col = col, pch = pch, bg = bg)
+}
 
 sbma <- function (sample1, sample2, absolute=TRUE) {
 		if(class(sample1) == "fast99") 
 				return(data.frame(Di=sbma(sample1$D1, sample2$D1), Dt=sbma(sample1$Dt, sample2$Dt)))
-		if(class(sample1) == "sobol2007") 
+		if(class(sample1) == "soboljansen") 
 				return(data.frame(S=sbma(sample1$S$original, sample2$S$original), T=sbma(sample1$T$original, sample2$T$original)))
 		if(class(sample1) == "LHS") {
 				x1 <- sample1@prcc$PRCC$original; 
@@ -133,31 +170,23 @@ sbma <- function (sample1, sample2, absolute=TRUE) {
 }
 
 setClass("LHS", representation=representation(
-											  N="numeric", data="matrix", factors="character", q="character", 
-											  q.arg="list", COR="matrix", eps="numeric", model="function", res="matrix",
+											  N="numeric", data="data.frame", factors="character", q="character", 
+											  q.arg="list", COR="matrix", eps="numeric", model="function", res="data.frame",
 											  res.names="character", prcc = "list")
 )
 
 get.results <- function(LHS) { return (LHS@res) }
 get.data <- function(LHS) {return (LHS@data) }
+
 setMethod(
 		  f="pcc",
 		  signature("LHS"),
 		  definition = function (X, y=NULL, rank=TRUE,nboot=1000, conf=0.95) {
 				  require(sensitivity)
-				  iprcc <- sensitivity::pcc(X@data, as.vector(X@res), nboot=nboot, rank=rank, conf=conf)
-				  return(iprcc);
+				  f <- function(res) sensitivity::pcc(X@data, as.vector(res), nboot=nboot, rank=rank, conf=conf)
+				  return(apply(X@res, 2, f))
 		  }
 		  )
-
-# setMethod(
-#           f="ecdf",
-#           signature("LHS"),
-#           definition = function (x) {
-#                   return(ecdf(x@res));
-#           }
-#           )
-
 
 plotecdf <- function (LHS, ...) {
 		nres <- dim(LHS@res)[2]
@@ -170,53 +199,33 @@ plotecdf <- function (LHS, ...) {
 			plot(ecdf(LHS@res[,i]), xlim=c(min, max), main='', xlab=LHS@res.names[i])
 }
 
-setMethod(
-		  f="kruskal.test",
-		  signature("LHS"),
-		  definition = function (x, Ncats=10, do.plot=TRUE, ...) {
-				  k <- NA;
-				  df <- Ncats-1;
-				  for (i in 1:length(x@data)) {
-						  cats <- cut(x@data[,i], breaks=quantile(x@data[,i], seq(0,1,1/Ncats)))
-						  k[i]<-(kruskal.test(x@res~cats))$statistic;
-				  }
-				  names(k) <- x@factors;
-				  if(do.plot) {
-						  plot(k, ylim=c(0, max(k)*1.1), xaxt='n')
-						  axis(1, at=1:length(x@data), labels=x@factors)
-						  abline(h=qchisq(0.05, df=df), lty=2)
-				  }
-				  return (k);
-		  }
-		  )
+plotprcc <- function (LHS, bg ='orange', ...) {
+		nres <- dim(LHS@res)[2]
+		nl <- floor(sqrt(nres))
+		nc <- ceiling(nres/nl)
+		opar <- par(mfrow=c(nl,nc), ...)
+	for(i in 1:nres) 
+	{
+			plot.pcc(LHS@prcc[[i]], bg=bg, main=LHS@res.names[[i]])
+			abline(h=0, lty=2)
+	}
+}
 
-LHS <- function (model, factors, N, q, q.arg, res.names, COR=matrix(0, length(factors), length(factors)), eps=0.0005, nboot=0) {
-		L <- matrix(nrow=N, ncol=length(factors));
+LHS <- function (model, factors, N, q, q.arg, res.names=NULL, COR=matrix(0, length(factors), length(factors)), eps=0.0005, nboot=0) {
+		L <- as.data.frame(matrix(nrow=N, ncol=length(factors)));
 		colnames(L) <- factors
 		for (i in 1:length(factors)) 
 				L[,i] <- sample(do.call(q[[i]], c(list(p = 1:N/N-1/N/2), q.arg[[i]])))
 		L <- LHScorcorr(L, COR,eps=eps); 
 		res <- t(model(L));
-		#prcc <- pcc(L, res, rank=TRUE);
-		prcc <- "to be implemented"
-		class(prcc) <- "list";
+		if (dim(res)[1] == 1) res <- t(res); # Caso seja retornado um unico valor
+		res <- as.data.frame(res);
+		if(!is.null(res.names))
+			colnames(res) <- res.names
+		# Calculates the PRCC for each response variable
+		f <- function(r) sensitivity::pcc(L, r, nboot=nboot, rank=T)
+		prcc <- apply(res, 2, f);
+		###
 		X <- new(Class="LHS", N=N, data=L,factors=factors, q=q, q.arg=q.arg, COR=COR, eps=eps, model=model, res=res, prcc=prcc, res.names=res.names);
 		return(X);
 }
-
-LHSextend<- function (oldLHS) {
-		newL <- as.data.frame(matrix(nrow=2*oldLHS@N, ncol=length(oldLHS@factors)));
-		names(newL) <- oldLHS@factors
-		for (i in 1:length(oldLHS@factors)) 
-				newL[,i] <- do.call(oldLHS@q[[i]], c(list(p =c((1:N)/N - 5/N/6, (1:N)/N - 1/N/6)), oldLHS@q.arg[[i]]))
-		newL <- LHScorcorr(newL, COR=oldLHS@COR, eps=oldLHS@eps);
-		newres <- oldLHS@model(newL);
-		oldLHS@N <- oldLHS@N*3;
-		oldLHS@data <- rbind(oldLHS@data, newL);
-		oldLHS@res <- c(oldLHS@res, newres);
-		prcc <- pcc(oldLHS@data, as.vector(oldLHS@res), rank=TRUE)
-		class(prcc) <- "list";
-		oldLHS@prcc <- prcc
-		return(oldLHS);
-}
-
