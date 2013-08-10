02f12d9793a57af91c95654e0a1e8b44a4fcc18f
Andre Chalom <andrechalom@gmail.com>
2013-08-10 08:55:10 +0000

    X
    git-svn-id: svn://www.lage.ib.usp.br/chalom/pse@1067 3f9c3246-d5c1-4856-9bbf-d06180df4c92

--- new/temporais-simula.R
+++ new/temporais-simula.R
@@ -1,15 +1,20 @@
+##### Matriz com 3x3: reverter para versao 1066
 library(MASS)
 library(bbmle)
 library(optimx)
 source("plot-profmle.r")
+library(expm)
 ### Matriz "correta"
-m <- matrix(c(0.3, 0, 1.5, 0.3, 0.4, 0, 0, 0.4, 0.6), ncol=3, byrow=T)
+m <- matrix(c(0.3, 1.45, 0.3, 0.4), ncol=2, byrow=T)
+#### | P1 | F  |
+#### | G1 | P2 |
 # Simulando uma serie temporal
-n0 <- c(22, 53, 81)
-temp <- data.frame(time=c(0,0,0), stage=c(1,2,3), density=n0)
-for (i in 1:20) {
-	n0 <- m%*%n0
+n0 <- c(90, 43)
+temp <- data.frame(time=c(0,0), stage=c(1,2), density=n0)
+for (i in 1:200) {
+	D <- matrix(c(rnorm(1, 0, 0.05), 0, 0, rnorm(1,0,0.1)), nrow=2, byrow=T)
+	n0 <- expm(D)%*%m%*%n0
 	temp <- rbind(temp, c(i, 1, n0[1]))
 	temp <- rbind(temp, c(i, 2, n0[2]))
-	temp <- rbind(temp, c(i, 3, n0[3]))
 }
+plot(temp[temp$stage==1,3])
--- new/temporais.R
+++ new/temporais.R
@@ -10,25 +10,27 @@ m <- function (t) log(n(t))
 logistic <- function (p) exp(p)/(1+exp(p))
 logit <- function (p) log(p) - log(1-p)
 
-nLL = function(v1=1, v2=1, v3=1, c12=0.7, c13=0, c23=0, p1=-50, g1=2, p2=0, g2=0, p3=2, f=-10) {
+nLL = function(v1=0.05, v2=0.01, c12=0, p1=logit(0.3), g1=logit(0.3), p2=logit(0.4), f=log(1.45)) {
 	p1 <- exp(p1)/(1+exp(p1))
 	p2 <- exp(p2)/(1+exp(p2))
-	p3 <- exp(p3)/(1+exp(p3))
 	g1 <- exp(g1)/(1+exp(g1))
-	g2 <- exp(g2)/(1+exp(g2))
 	f <- exp(f)
-	hm <- function (t) log(c(p1*n(t)[1]+f*n(t)[3], g1*n(t)[1]+p2*n(t)[2], g2*n(t)[2] + p3*n(t)[3]))
-	Sigma = matrix(c(v1, c12, c13, c12, v2, c23, c13, c23, v3), nrow=3)
+	hm <- function (t) log(c(p1*n(t)[1]+f*n(t)[2], g1*n(t)[1]+p2*n(t)[2]))
+	Sigma = matrix(c(v1, c12, c12, v2), nrow=2)
 	total = 0
 	for (i in 1:max(temp$time)) total = total + (m(i) - hm(i)) %*% ginv(Sigma) %*% (m(i) - hm(i))
-	print (matrix(c(p1, 0, f, g1, p2, 0, 0, g2, p3), ncol=3, byrow=T))
-	return(as.numeric(5*log(det(Sigma)) + total))
+	print (matrix(c(p1, f, g1, p2), ncol=2, byrow=T))
+	return(as.numeric(max(temp$time)*log(det(Sigma)) + total))
 }
 
-model <- mle2(nLL, optimizer="optimx",
-			  start=list(p1=logit(0.3), g1=logit(0.3), f=log(1.5), g2=logit(0.4), p2=logit(0.4), p3=logit(0.6)),
-			  	   fixed=list(v1=0.1,v2=0.1,v3=0.1,
-							  c12=0,c13=0,c23=0
-							  ))
-model
-plot.profmle(profile(model))
+#model <- mle2(nLL, optimizer="optimx", fixed=list(v1=0.05, v2=0.01, c12=0),
+#			  lower=c(-5, -5, -5, -5), upper=c(5,5,5,5))
+#model
+#plot.profmle(profile(model))
+
+vnLL <- Vectorize(nLL)
+par(mfrow=c(2,2))
+plot(logistic(seq(-1.5,0, by=0.1)), vnLL(p1=seq(-1.5,0,by=0.1)), type='l', main="p1")
+plot(exp(seq(-1,1.2,by=0.1)),vnLL(f=seq(-1,1.2,by=0.1)), type='l', main="f")
+plot(logistic(seq(-1.5,0, by=0.1)), vnLL(g1=seq(-1.5,0,by=0.1)), type='l', main="g1")
+plot(logistic(seq(-1,0.4,by=0.1)),vnLL(p2=seq(-1,0.4,by=0.1)), type='l', main="p2")
