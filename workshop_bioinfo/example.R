13b95b9e78f149c2ce22e67066f19c3701b0cbf9
Andre Chalom <andrechalom@gmail.com>
2013-12-02 19:56:41 +0000

    X
    git-svn-id: svn://www.lage.ib.usp.br/chalom/pse@1324 3f9c3246-d5c1-4856-9bbf-d06180df4c92

--- workshop_bioinfo/example.R
+++ workshop_bioinfo/example.R
@@ -1,3 +1,7 @@
 library(pse)
-l <- LHS(model=function(x) {x[,1]+x[,2]*x[,3]+x[,4]*x[,5]/x[,6]+rnorm(1,1,1)}, factors=7, N=200, nboot=50)
+l <- LHS(model=function(x) {x[,1]+x[,2]*x[,3]+x[,4]*x[,5]/(1+x[,6])+rnorm(1,1,0.3)}, factors=7, N=200, nboot=50, res.names="Model results", repetitions=5)
 plotprcc(l)
+plotecdf(l)
+plotscatter(l)
+plotcv(l)
+
